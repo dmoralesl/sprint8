b3977e5bfb15b87580c3176638771a3ddc44d32b
David Morales <5dmorales474@gmail.com>
2021-11-17 12:52:42 +0100

    Exercise 8 - Films view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { CustomInterceptor } from './interceptors';
 import { DetailComponent } from './components/detail/detail.component';
+import { FilmComponent } from './components/film/film.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
+import { PilotComponent } from './components/pilot/pilot.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SignupComponent } from './components/signup/signup.component';
 import { StarshipsComponent } from './components/starships/starships.component';
-import { PilotComponent } from './components/pilot/pilot.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { PilotComponent } from './components/pilot/pilot.component';
     LoginComponent,
     SignupComponent,
     PilotComponent,
+    FilmComponent,
   ],
   imports: [
     BrowserModule,
